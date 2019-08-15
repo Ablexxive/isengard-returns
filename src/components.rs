41e40c7118c03ff10e8c19b5e17d834ef412e398
Gabriel Martinez <reitaka@gmail.com>
2019-08-14 18:58:55 -0700

    Move resource types to a new file

--- src/components.rs
+++ src/components.rs
@@ -7,12 +7,6 @@ use specs::{
     Component,
 };
 
-#[derive(Default)]
-pub struct DeltaTime(pub f32);
-
-#[derive(Default)]
-pub struct YouLose(pub bool);
-
 #[derive(Clone, Copy, Debug, Component)]
 pub struct Transform {
     pub position: Point2<f32>,
